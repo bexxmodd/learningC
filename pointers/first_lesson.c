3424ac4345fe554521100fc6253f63cd4657072f
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-03-17 22:29:48 -0400

    pointers freeCodeAcademy course

---
+++ pointers/first_lesson.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// int allocates 4 bytes of memory in RAM
+// char - 1 byte
+// float - 4 bytes
+
+
+int main()
+{
+    int a;
+
+    int *p; // defines pointer "p" which will point to some address in memory.
+    p = &a; // "&" in front of a variables gives us the address of that variable e.g. returns a pointer.
+
+    a = 19;
+
+    printf("%d\n", *p); // prints the value of variable pointer `p points to (in this case it's `a`)
+    printf("%d\n", *p); // this is called derefrencing - we'll print a value at the address `p` points to.
+    printf("%d\n", &a); // will print the memory address of where `a` is stored
+
+    printf("%d\n", a);
+    *p = 13; // dereferencing - value at address being pointed by `p` is equal 12
+    printf("%d\n", a); // value will be now changed
+
+
+
+    return 0;
+}
+
