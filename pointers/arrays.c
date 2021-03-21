8dce85be1bb496373c3ada67f8a01c59ee7a8e81
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-03-21 13:27:58 -0400

    some new exercises

--- pointers/arrays.c
+++ pointers/arrays.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int sumi(int arr[], int size);
+void println(const char *line);
 
 int main()
 {
@@ -13,6 +14,9 @@ int main()
     printf("---------------\n");
     printf("Sum of array is: %d\n", sumi(arr, s));
 
+    char A[100] = "Immutable line";
+    println(A);
+
     return 0;
 }
 
@@ -22,4 +26,13 @@ int sumi(int arr[], int size)
     for (i = 0; i < size; i++)
         sum += arr[i];
     return sum;
+}
+
+/* prints the given line of string
+`const` forces line to have only read access */
+void println(const char *line)
+{
+    while (*line != '\0')
+        printf("%c", *line++);
+    printf("\n");
 }
