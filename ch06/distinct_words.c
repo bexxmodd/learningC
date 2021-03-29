56cc156297f61659b26629ed52dddd6ddc452eee
Bexx Modebadze <bexxmodd@seas.upenn.edu>
2021-03-29 07:49:44 -0400

    corrected dict like struct

--- ch06/distinct_words.c
+++ ch06/distinct_words.c
@@ -11,25 +11,21 @@ int main(int argc, char** argv)
 {
     int i, j;
 
-    dict* arr[100];
-    dict a = {argv[0], 1};
-    arr[0] = &a;
+    dict hello = {"Hello", 1};
+    dict world = {"world", 1};
+    dict once = {"once", 1};
+    dict again = {"again", 1};
+    dict arr[] = {hello, world, once, again};
     size_t size = sizeof(arr) / sizeof(arr[0]);
 
     for (i = 1; i < argc; i++) {
         for (j = 0; j < size; j++) {
-            if (strcmp(argv[i], arr[j]->word) == 0) {
-                printf("found");
-            } else {
-                arr[j] = (dict*) malloc(sizeof(dict));
-                arr[j]->word = argv[i];
-                arr[j]->count = 1;
-            }
+            if (strcmp(argv[i], arr[j].word) == 0)
+                arr[j].count++;
         }
-
     }
-    for (i = 0; i < 5; i++)
-    printf("word: %s count: %d\n", arr[i]->word, arr[i]->count);
+    for (i = 0; i < 4; i++)
+        printf("word: %s count: %d\n", arr[i].word, arr[i].count);
     
     return 0;
 }
